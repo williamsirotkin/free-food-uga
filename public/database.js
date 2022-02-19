c7768872c7c7c67e5928251de2c9fdec2e890963
stephennemeth4 <sjn57152@uga.edu>
2022-02-18 22:16:45 -0500

    I Did something

---
+++ public/database.js
@@ -0,0 +1,9 @@
+const db = getDatabase();
+const ref = db.ref('server/saving-data/fireblog/posts');
+
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on('value', (snapshot) => {
+  console.log(snapshot.val());
+}, (errorObject) => {
+  console.log('The read failed: ' + errorObject.name);
+});
