a9d28e4d948ef66585961a5605a472c6f16f4ad4
RaymondFeckoury <rjf21434@uga.edu>
2022-02-19 02:50:58 -0500

    Pulling both latitude and longitude values when add location button is clicked.

--- public/pullMarker.js
+++ public/pullMarker.js
@@ -1,5 +1,13 @@
-function pullLat() {
+function pullCoordinates() {
+    pullLong();
     var value = document.getElementById('lat');
     var dbRef = firebase.database().ref().child('Buildings').child('Boyd').child('Latitude');
+    dbRef.on('value', snap => value.innerText = snap.val());
+}
+
+function pullLong() {
+    var value = document.getElementById('long'); // optional
+    // pass this in to the write function. This var contains the coordinate.
+    var dbRef = firebase.database().ref().child('Buildings').child('Boyd').child('Longitude');
     dbRef.on('value', snap => value.innerText = snap.val());
 }
