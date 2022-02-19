4b1ec5303548836422010d26d3dca77b152007f1
stephennemeth4 <sjn57152@uga.edu>
2022-02-19 15:34:21 -0500

    Update write.js

--- public/write.js
+++ public/write.js
@@ -1,38 +1,42 @@
-var latitude = 50.0;
-var longitude = 50.0;
+var latitude = 50;
+var longitude = 50;
 function writeMarker() {
     var select = document.getElementById('locationInput');
     var building = select.options[select.selectedIndex].value;
-    await getLat(building);
-    var food = select.options[select.selectedIndex].value;
-    var duration = select.options[select.selectedIndex].value;
-    var eventType = document.getElementById('eventType').value;
-    var additional = document.getElementById('additionalType').value;
-    var data = {
-        Building : building,
-        Food : food,
-        Duration: duration,
-        Event : eventType,
-        Additional : additional,
-        Latitude : latitude,
-        Longitude: longitude
-    }
-    console.log(data);
-    var firebaseRef = firebase.database().ref('Marker');
-    firebaseRef.push(data);
-    initMap();
+    getLat(building);
 }
 
 async function getLat(building) {
-    getLong(building);
     var dbRef1 = firebase.database().ref().child('Buildings').child(building).child('Latitude');
     dbRef1.on('value', snap => {
         latitude = snap.val();
+        dbRef1 = firebase.database().ref().child('Buildings').child(building).child("Longitude");
+        dbRef1.on('value', snap => {
+            longitude = snap.val();
+            var select = document.getElementById("foodType");
+            var food = select.options[select.selectedIndex].value;
+            select = document.getElementById("duration")
+            var duration = select.options[select.selectedIndex].value;
+            var eventType = document.getElementById('eventType').value;
+            var additional = document.getElementById('additionalType').value;
+            var data = {
+                Building : building,
+                Food : food,
+                Duration: duration,
+                Event : eventType,
+                Additional : additional,
+                Latitude : latitude,
+                Longitude: longitude
+            }
+            console.log(data);
+            var firebaseRef = firebase.database().ref('Marker');
+            firebaseRef.push(data);
+            initMap();
+        })
     });
 }
 
 async function getLong(building) {
-    await getLat();
     var dbRef2 = firebase.database().ref().child('Buildings').child(building).child('Longitude');
     var long = dbRef2.on('value', snap => {
         longitude = snap.val();
