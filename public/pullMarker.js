8a6279ca185e212f3c3d9db74a2c50a15fe16a39
stephennemeth4 <sjn57152@uga.edu>
2022-02-19 09:59:10 -0500

    Wrote write.js

--- public/pullMarker.js
+++ public/pullMarker.js
@@ -1,21 +1,24 @@
 
-function pullLat(building) {
-    var lat;
+async function pullLat(building) {
     var select = document.getElementById('locationInput');
     var building = select.options[select.selectedIndex].value;
     var value = document.getElementById('lat');
     var dbRef = firebase.database().ref().child('Buildings').child(building).child('Latitude');
-    dbRef.on('value', snap => lat = snap.val());
-    return lat;
+    var latitude = await dbRef.on('value', snap => {
+        return snap.val();
+    });
+    return latitude;
 }
 
-function pullLong(building) {
+async function pullLong(building) {
     var long;
     var select = document.getElementById('locationInput');
     var building = select.options[select.selectedIndex].value;
     var value = document.getElementById('long'); // optional
     // pass this in to the write function. This var contains the coordinate.
     var dbRef = firebase.database().ref().child('Buildings').child(building).child('Longitude');
-    dbRef.on('value', snap => long = snap.val());
-    return long;
+    var longitude = await dbRef.on('value', snap => {
+        return snap.val();
+    });
+    return longitude;
 }
--- public/write.js
+++ public/write.js
@@ -1,5 +1,30 @@
 function writeMarker() {
-    alert("The Button was clicked");
-    var x = document.getElementById("testText").val;
+    var index = markers.length;
+    var string = "Markers/" + index
+    var firebaseRef = firebase.database().ref(string);
+    var select = document.getElementById('locationInput');
+    var building = select.options[select.selectedIndex].value;
+    var x = pullLat(building);
+    var y = pullLong(building);
+    select = document.getElementById('foodType');
+    var food = select.options[select.selectedIndex].value;
+    select = document.getElementById('duration');
+    var duration = select.options[select.selectedIndex].value;
+    var eventType = document.getElementById('eventType').value;
+    var additional = document.getElementById('additionalType');
 
+    firebaseRef = firebase.database().ref(string + "/Location");
+    firebaseRef.push(building);
+    firebaseRef = firebase.database().ref(string + "/Latitude");
+    firebaseRef.push(x);
+    firebaseRef = firebase.database().ref(string + "/Longitude");
+    firebaseRef.push(y);
+    firebaseRef = firebase.database().ref(string + "/Food");
+    firebaseRef.push(food);
+    firebaseRef = firebase.database().ref(string + "/Duration");
+    firebaseRef.push(duration);
+    firebaseRef = firebase.database().ref(string + "/Event");
+    firebaseRef.push(eventType);
+    firebaseRef = firebase.database().ref(string + "/Additional");
+    firebaseRef.push(additional);
 }
