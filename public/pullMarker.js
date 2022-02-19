bf85956efa54195dc09b081273617415f44d930c
RaymondFeckoury <rjf21434@uga.edu>
2022-02-19 03:36:33 -0500

    Added return types so that write function can access the coordinates.

--- public/pullMarker.js
+++ public/pullMarker.js
@@ -1,18 +1,21 @@
-function getBuildingID() {
+
+function pullLat(building) {
+    var lat;
     var select = document.getElementById('locationInput');
     var building = select.options[select.selectedIndex].value;
-    pullCoordinates(building);
-}
-function pullCoordinates(building) {
-    pullLong(building);
     var value = document.getElementById('lat');
     var dbRef = firebase.database().ref().child('Buildings').child(building).child('Latitude');
-    dbRef.on('value', snap => value.innerText = snap.val());
+    dbRef.on('value', snap => lat = snap.val());
+    return lat;
 }
 
 function pullLong(building) {
+    var long;
+    var select = document.getElementById('locationInput');
+    var building = select.options[select.selectedIndex].value;
     var value = document.getElementById('long'); // optional
     // pass this in to the write function. This var contains the coordinate.
     var dbRef = firebase.database().ref().child('Buildings').child(building).child('Longitude');
-    dbRef.on('value', snap => value.innerText = snap.val());
+    dbRef.on('value', snap => long = snap.val());
+    return long;
 }
